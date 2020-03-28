6c2fd1ee8d1103b1480ddd275f6982e3fa298ee0
Ajay P Anand <ajaypanand007@gmail.com>
2020-03-28 20:36:13 +0530

    Updates

---
+++ precache-manifest.30ea27ec0da0ec8024ca697e6cfaf3da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870275569c8721766fc298399c7d4882",
+    "url": "/Tes/index.html"
+  },
+  {
+    "revision": "20361936f58aa41ba73a",
+    "url": "/Tes/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "ae49557f5d1bef243983",
+    "url": "/Tes/static/css/main.043a352b.chunk.css"
+  },
+  {
+    "revision": "20361936f58aa41ba73a",
+    "url": "/Tes/static/js/2.54e5a7b7.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/Tes/static/js/2.54e5a7b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae49557f5d1bef243983",
+    "url": "/Tes/static/js/main.81e35b29.chunk.js"
+  },
+  {
+    "revision": "6514461998a74a9881fc",
+    "url": "/Tes/static/js/runtime-main.6198a0e4.js"
+  }
+]);
